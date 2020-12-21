05642da5b176c5bb059c0b0f9327118820d5c576
Frederik Zipp <fzipp@gmx.de>
2020-12-21 16:43:21 +0100

    add benchmark

---
+++ benckmark_test.go
@@ -0,0 +1,82 @@
+// Copyright 2020 Frederik Zipp. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package canvas
+
+import (
+	"image"
+	"image/color"
+	"testing"
+)
+
+func BenchmarkContext(b *testing.B) {
+	draws := make(chan []byte)
+	go func() {for range draws {}}()
+	ctx := newContext(draws, nil, nil, config{})
+	for n := 0; n < b.N; n++ {
+		ctx.SetFillStyle(color.White)
+		ctx.SetFillStyleString("green")
+		ctx.SetFillStyleGradient(&Gradient{})
+		ctx.SetFillStylePattern(&Pattern{})
+		ctx.SetFont("bold 48px serif")
+		ctx.SetGlobalAlpha(1)
+		ctx.SetGlobalCompositeOperation(OpDestinationOut)
+		ctx.SetImageSmoothingEnabled(true)
+		ctx.SetLineCap(CapRound)
+		ctx.SetLineDashOffset(1)
+		ctx.SetLineJoin(JoinBevel)
+		ctx.SetLineWidth(1)
+		ctx.SetMiterLimit(1)
+		ctx.SetShadowBlur(1)
+		ctx.SetShadowColor(color.White)
+		ctx.SetShadowColorString("#ffa0b3")
+		ctx.SetShadowOffsetX(1)
+		ctx.SetShadowOffsetY(1)
+		ctx.SetStrokeStyle(color.White)
+		ctx.SetStrokeStyleString("yellow")
+		ctx.SetStrokeStyleGradient(&Gradient{})
+		ctx.SetStrokeStylePattern(&Pattern{})
+		ctx.SetTextAlign(AlignLeft)
+		ctx.SetTextBaseline(BaselineBottom)
+		ctx.Arc(1, 1, 1, 1, 1, true)
+		ctx.ArcTo(1, 1, 1, 1, 1)
+		ctx.BeginPath()
+		ctx.BezierCurveTo(1, 1, 1, 1, 1, 1)
+		ctx.ClearRect(1, 1, 1, 1)
+		ctx.Clip()
+		ctx.ClosePath()
+		ctx.Ellipse(1, 1, 1, 1, 1, 1, 1, true)
+		ctx.Fill()
+		ctx.FillRect(1, 1, 1, 1)
+		ctx.FillText("hello, world", 1, 1)
+		ctx.FillTextMaxWidth("hello, world", 1, 1, 1)
+		ctx.LineTo(1, 1)
+		ctx.MoveTo(1, 1)
+		ctx.QuadraticCurveTo(1, 1, 1, 1)
+		ctx.Rect(1, 1, 1, 1)
+		ctx.Restore()
+		ctx.Rotate(1)
+		ctx.Save()
+		ctx.Scale(1, 1)
+		ctx.Stroke()
+		ctx.StrokeText("hello, world", 1, 1)
+		ctx.StrokeTextMaxWidth("hello, world", 1, 1, 1)
+		ctx.StrokeRect(1, 1, 1, 1)
+		ctx.Translate(1, 1)
+		ctx.Transform(1, 1, 1, 1, 1, 1)
+		ctx.SetTransform(1, 1, 1, 1, 1, 1)
+		ctx.SetLineDash([]float64{1, 1, 1})
+		ctx.CreateImageData(image.NewRGBA(image.Rect(0, 0, 0, 0)))
+		ctx.PutImageData(&Image{}, 1, 1)
+		ctx.PutImageDataDirty(&Image{}, 1, 1, 1, 1, 1, 1)
+		ctx.DrawImage(&Image{}, 1, 1)
+		ctx.DrawImageScaled(&Image{}, 1, 1, 1, 1)
+		ctx.DrawImageSubRectangle(&Image{}, 1, 1, 1, 1, 1, 1, 1, 1)
+		ctx.CreateLinearGradient(1, 1, 1, 1)
+		ctx.CreateRadialGradient(1, 1, 1, 1, 1, 1)
+		ctx.CreatePattern(&Image{}, PatternRepeat)
+		ctx.Flush()
+	}
+	close(draws)
+}
