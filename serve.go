2a11f600fca7d6bf9585f9e6745e164dc5e04bc1
Frederik Zipp <fzipp@gmx.de>
2021-01-06 23:03:43 +0100

    move event decoding functions to event.go

--- serve.go
+++ serve.go
@@ -8,10 +8,8 @@
 package canvas
 
 import (
-	"errors"
 	"html/template"
 	"log"
-	"math"
 	"net/http"
 	"sync"
 	"time"
@@ -141,115 +139,4 @@ func readMessages(conn *websocket.Conn, events chan<- Event, wg *sync.WaitGroup)
 		}
 		events <- event
 	}
-}
-
-const (
-	evMouseMove byte = 1 + iota
-	evMouseDown
-	evMouseUp
-	evKeyPress
-	evKeyDown
-	evKeyUp
-	evClick
-	evDblClick
-	evAuxClick
-	evWheel
-	evTouchStart
-	evTouchMove
-	evTouchEnd
-	evTouchCancel
-)
-
-func decodeEvent(p []byte) (Event, error) {
-	eventType := p[0]
-	switch eventType {
-	case evMouseMove:
-		return MouseMoveEvent{decodeMouseEvent(p)}, nil
-	case evMouseDown:
-		return MouseDownEvent{decodeMouseEvent(p)}, nil
-	case evMouseUp:
-		return MouseUpEvent{decodeMouseEvent(p)}, nil
-	case evKeyPress:
-		return KeyPressEvent{decodeKeyboardEvent(p)}, nil
-	case evKeyDown:
-		return KeyDownEvent{decodeKeyboardEvent(p)}, nil
-	case evKeyUp:
-		return KeyUpEvent{decodeKeyboardEvent(p)}, nil
-	case evClick:
-		return ClickEvent{decodeMouseEvent(p)}, nil
-	case evDblClick:
-		return DblClickEvent{decodeMouseEvent(p)}, nil
-	case evAuxClick:
-		return AuxClickEvent{decodeMouseEvent(p)}, nil
-	case evWheel:
-		return decodeWheelEvent(p), nil
-	case evTouchStart:
-		return TouchStartEvent{decodeTouchEvent(p)}, nil
-	case evTouchMove:
-		return TouchMoveEvent{decodeTouchEvent(p)}, nil
-	case evTouchEnd:
-		return TouchEndEvent{decodeTouchEvent(p)}, nil
-	case evTouchCancel:
-		return TouchCancelEvent{decodeTouchEvent(p)}, nil
-	}
-	return nil, errors.New("unknown event type: '" + string(eventType) + "'")
-}
-
-func decodeMouseEvent(p []byte) MouseEvent {
-	return MouseEvent{
-		Buttons:      MouseButtons(p[1]),
-		X:            int(byteOrder.Uint32(p[2:])),
-		Y:            int(byteOrder.Uint32(p[6:])),
-		modifierKeys: modifierKeys(p[10]),
-	}
-}
-
-func decodeKeyboardEvent(p []byte) KeyboardEvent {
-	keyStringLength := int(byteOrder.Uint32(p[2:]))
-	return KeyboardEvent{
-		Key:          string(p[6 : 6+keyStringLength]),
-		modifierKeys: modifierKeys(p[1]),
-	}
-}
-
-func decodeWheelEvent(p []byte) WheelEvent {
-	return WheelEvent{
-		MouseEvent: decodeMouseEvent(p),
-		DeltaX:     math.Float64frombits(byteOrder.Uint64(p[11:])),
-		DeltaY:     math.Float64frombits(byteOrder.Uint64(p[19:])),
-		DeltaZ:     math.Float64frombits(byteOrder.Uint64(p[27:])),
-		DeltaMode:  DeltaMode(p[35]),
-	}
-}
-
-func decodeTouchEvent(p []byte) TouchEvent {
-	touches, p := decodeTouchList(p[1:])
-	changedTouches, p := decodeTouchList(p)
-	targetTouches, p := decodeTouchList(p)
-	return TouchEvent{
-		Touches:        touches,
-		ChangedTouches: changedTouches,
-		TargetTouches:  targetTouches,
-		modifierKeys:   modifierKeys(p[0]),
-	}
-}
-
-func decodeTouchList(p []byte) (TouchList, []byte) {
-	length := p[0]
-	p = p[1:]
-	list := make(TouchList, length)
-	for i := range list {
-		var t Touch
-		t, p = decodeTouch(p)
-		list[i] = t
-	}
-	return list, p
-}
-
-func decodeTouch(p []byte) (Touch, []byte) {
-	return Touch{
-		Identifier: byteOrder.Uint32(p),
-		X:          int(byteOrder.Uint32(p[4:])),
-		Y:          int(byteOrder.Uint32(p[8:])),
-	}, p[12:]
 }
